bb868eeaa0de5c05962be40ffc58d28afad568fe
piruty <8793641+piruty@users.noreply.github.com>
2022-05-10 16:18:20 +0900

    clickを追加

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 from os import getenv
 
 import requests
+import click
 
-KEY = getenv('YOUTUBE_API_KEY')
+
 YOUTUBE_URL = 'https://www.googleapis.com/youtube/v3/search'
 
-payload = {
-    'key': KEY,
-    'part': 'snippet',
-    'type': 'video',
-    'maxResult': 5,
-    'q': 'hikakin'
-}
 
-r = requests.get(YOUTUBE_URL, params=payload)
-print(r.json())
+@click.command()
+@click.option('--key', type=str, default='', help='Youtube Key')
+@click.option('--count', type=int, default=5, help='Result item count')
+@click.option('--query', type=str, default='', help='Search Query')
+def search_youtube(key: str, count: int, query: str):
+    if key == '':
+        key = getenv('YOUTUBE_API_KEY')
+
+    payload = {
+        'key': key,
+        'part': 'snippet',
+        'type': 'video',
+        'maxResults': count,
+        'order': 'videoCount'
+    }
+
+    r = requests.get(YOUTUBE_URL, params=payload)
+    click.echo(r.json())
+
+if __name__ == '__main__':
+    search_youtube()
